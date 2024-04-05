ad13521c29f032c4c6ba77d92d74ab1be78d9ef9
Brijesh_Amrutiya <brijesh.qtonz@yahoo.com>
2024-04-05 19:43:22 +0530

    Mutation implemented

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const resolvers = {
         games() {
             return db.games
         },
-        game(_,args){
-            return db.games.find((game)=> game.id === args.id)
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id)
         },
         authors() {
             return db.authors
@@ -38,8 +38,33 @@ const resolvers = {
         reviews() {
             return db.reviews
         },
-        review(_,args){
-            return db.reviews.find((review)=> review.id === args.id)
+        review(_, args) {
+            return db.reviews.find((review) => review.id === args.id)
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id == parent.id)
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
+        }
+    },
+    Mutation: {
+        deleteGame(_, args){
+            db.games = db.games.filter((d)=>d.id !== args.id)
+
+            return db.games
         }
     }
 }
